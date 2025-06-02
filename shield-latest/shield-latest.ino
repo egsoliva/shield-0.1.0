fd4747989c70db05ba9de573ffb023d483fd4f84
tsukk1 <71861992+sagaciousboi@users.noreply.github.com>
2025-06-02 23:12:31 +0800

    Add files via upload

---
+++ shield-latest/shield-latest.ino
@@ -0,0 +1,165 @@
+/**********************************************************************************************************
+*
+*   SHIELD v1.0 - Smart Hard Hat with Impact Emergency Location Detector for Instant Disaster Response
+*
+*   COMMENTS:
+*     - This version has no threshold and only focuses on setting up the SIM800L EVB module, GPS module,
+*       and the ADXL345 module (a temporary threshold is placed to test SMS and calls)
+*     - The pins for the modules and the buzzer are defined in the code
+*     - The range set in the code varies from -16 to +16g. This is to allow readings from heavy impact due
+*       to debris or objects during an emergency
+*     - The data rate set in the code is 100 Hz
+*     - ADXL345 Datasheet: https://www.analog.com/media/en/technical-documentation/data-sheets/adxl345.pdf
+*     - Threshold should be clearly defined in the next update
+*     - Add a debug feature in the next update to monitor errors
+*     - Add pitch, roll, and yaw if possible to detect movement from head
+*
+**********************************************************************************************************/
+
+#include <Wire.h>
+#include <ADXL345.h>
+#include <TinyGPS++.h>
+#include <Filters.h>
+#include <AH/Timing/MillisMicrosTimer.hpp>
+#include <Filters/Butterworth.hpp>
+
+#define BUZZER 4
+#define SDA 23
+#define SCL 22
+#define GPS_RX 17
+#define GPS_TX 16
+#define SIM800L_RX 19
+#define SIM800L_TX 18
+#define GPS_BAUD 9600
+#define SIM800L_BAUD 9600
+
+TinyGPSPlus gps;
+HardwareSerial gpsSerial(2);
+HardwareSerial sim800Serial(1);
+
+ADXL345 accelerometer;
+
+float x, y, z, accel, x_filtered, y_filtered, z_filtered;
+
+const char *numbers[] = {"adviserNumber", "guardianNumber", "nurseNumber"}; // Place their numbers in the respective places
+
+double latitude = gps.location.lat();
+double longitude = gps.location.lng();
+
+const double f_s = 100; // Sample frequency (Hz)
+const double f_c = 10; // Cut-off frequency (Hz)
+const double f_n = 2 * f_c / f_s; // Normalized cut-off frequency (Hz)
+
+void setup() {
+  Serial.begin(115200);  
+  pinMode(BUZZER, OUTPUT);
+  Wire.begin(SDA, SCL);
+  accelerometer.begin();
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, GPS_RX, GPS_TX);
+  sim800Serial.begin(SIM800L_BAUD, SERIAL_8N1, SIM800L_RX, SIM800L_TX);
+  setAccelerometerSettings();
+}
+
+void loop() {
+  readAccelerometerData();
+  readGPSData();
+
+  if(accel > 10 && gpsSerial.available() > 0) {
+    tone(BUZZER, 4000, 5000); // You may adjust the parameters
+    sendEmergency();
+  }
+}
+
+// You may modify the range and data rate (other settings can be found on the ADXL345 library)
+void setAccelerometerSettings() {
+  accelerometer.setRange(ADXL345_RANGE_16G);
+  accelerometer.setDataRate(ADXL345_DATARATE_100HZ);
+}
+
+// Read data from accelerometer and filter noise
+void readAccelerometerData() {
+  Vector norm = accelerometer.readNormalize();
+  Vector filtered = accelerometer.lowPassFilter(norm, 0.78); // You may set the alpha as [0.1, 0.9]
+  auto filter = butter<6>(f_n); // Sixth-order Butterworth Filter (can be changed)
+  
+  x = norm.XAxis;
+  y = norm.YAxis;
+  z = norm.ZAxis;
+  x_filtered = filter(x); // Using Butterworth Filter (readings are stable, unless I messed up the const values)
+  y_filtered = filter(y); // Using Butterworth Filter (readings are too noisy, must be adjusted or use another filter)
+  z_filtered = filtered.ZAxis; // Uses a Low Pass Filter (readings are a little noisy)
+  
+  accel = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
+  Serial.print(millis()); Serial.print(","); // Represent milliseconds passed
+  Serial.print("X:"); Serial.print(x_filtered, 2);
+  Serial.print(" Y:"); Serial.print(y_filtered, 2);
+  Serial.print(" Z:"); Serial.print(z_filtered, 2);
+  Serial.print(" Accel:"); Serial.println(accel, 2);
+  delay(10); // Send data every 10ms or at 100 Hz
+}
+
+// Read data from GPS module (from TinyGPS++ library)
+void readGPSData() {
+  while(gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+    if (gps.location.isUpdated()) {
+      Serial.print("Latitude:"); Serial.print(latitude, 6);
+      Serial.print(" Longitude:"); Serial.print(longitude, 6);
+    }
+  }
+}
+
+void sendEmergency() {
+  sendSMS(numbers);
+  makeCall(numbers);
+}
+
+// SMS sending function
+void sendSMS(const char **numbers) {
+  for(int i = 0; i < 3; i++) {
+    sim800Serial.println("AT+CSQ");
+    delay(1000);
+
+    sim800Serial.println("AT");
+    delay(1000);
+    
+    sim800Serial.println("AT+CMGF=1"); // Set SMS mode to text
+    delay(1000);
+    
+    sim800Serial.print("AT+CMGS=\""); 
+    sim800Serial.print(numbers[i]); 
+    sim800Serial.println("\"");
+    delay(1000);
+
+    String latstr = String(latitude, 6);
+    String lngstr = String(longitude, 6);
+
+    String text = "EMERGENCY MESSAGE ALERT";
+    text += "SEND HELP IMMEDIATELY to my current location: ";
+    text += "Latitude= " + latstr + "\n";
+    text += "Longitude= " + lngstr + "\n";
+    text += "This is an automated message. Please do not reply. meow (^.v.^)/";
+
+    sim800Serial.print(text);
+    delay(500);
+    sim800Serial.write(26); // End message with Ctrl+Z
+    delay(1000);
+  }
+}
+
+// Send call function
+void makeCall(const char **numbers) {
+  for(int i = 0; i < 3; i++) {
+    sim800Serial.print("ATD");
+    sim800Serial.print(numbers[i]);
+    sim800Serial.println(";\r\n");
+
+    Serial.println("Making call...");
+    delay(8000);
+
+    // End call
+    sim800Serial.println(F("ATH"));
+    delay(1000);
+  }
+}
